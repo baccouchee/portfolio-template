26e1c70d43f0f61045d38be716d3342114a9dac9
BACCOUCHE Omar <omar.baccouche-extern@renault.com>
2023-07-19 15:56:15 +0200

    Boiler Plate

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Hero from './components/hero'
+import Analytics from './components/Analytics'
+
+export function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Hero />
+        <Analytics />
+      </div>
+    </>
+  )
+}
+
+export default App
