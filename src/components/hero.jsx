365678505b17b479b0bf36a33e1064ec3df79da9
BACCOUCHE Omar <omar.baccouche-extern@renault.com>
2023-07-19 16:21:19 +0200

    fix react-typed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Typed from 'react-typed'
 
 const Hero = () => {
   return (
@@ -9,13 +8,6 @@ const Hero = () => {
         <h1 className="md:text-6xl sm:text-5xl text-4xl font-bold md:py-6">Unleashing Possibilities.</h1>
         <div className="flex justify-center items-center">
           <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">Fast, flexible financing for</p>
-          <Typed
-            className="text-[5vw] font-bold md:pl-4 pl-2"
-            strings={['BTB', 'BTC', 'SASS']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500">
           Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.
