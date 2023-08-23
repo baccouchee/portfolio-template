0fb22b5042f6ee2ba0f2126aff2a6f367c7e8d2e
BACCOUCHE Omar <omar.baccouche-extern@renault.com>
2023-08-23 17:45:45 +0200

    cursor effect bug

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../css/Cursor.css'
 
 const Cursor = () => {
+  const [cursorPosition, setCursorPosition] = useState({ top: 0, left: 0 })
+
   useEffect(() => {
     const cursor = document.querySelector('.cursor')
 
     const handleMouseMove = e => {
-      cursor.setAttribute('style', `top: ${e.pageY - 10}px; left: ${e.pageX - 10}px;`)
+      setCursorPosition({
+        top: e.clientY + window.scrollY - 10,
+        left: e.clientX + window.scrollX - 10,
+      })
     }
 
     const handleClick = () => {
@@ -26,7 +31,14 @@ const Cursor = () => {
     }
   }, [])
 
-  return <div className="cursor" />
+  const windowWidth = window.innerWidth
+
+  // Conditionally render the cursor component based on the screen width
+  if (windowWidth <= 768) {
+    return null // Don't render the cursor on smaller screens
+  }
+
+  return <div className="cursor" style={{ top: cursorPosition.top + 'px', left: cursorPosition.left + 'px' }} />
 }
 
 export default Cursor
